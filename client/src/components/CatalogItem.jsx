7a1c4ecf73d19317f793649a492028fed703ccac
JivkoKarakashev <86545491+JivkoKarakashev@users.noreply.github.com>
2023-11-06 18:05:55 +0200

    catalog page update

--- client/src/components/CatalogItem.jsx
+++ client/src/components/CatalogItem.jsx
@@ -11,21 +11,21 @@ const CatalogItem = ({
 }) => {
     return (
         <li className={styles["item"]}>
-                    <header className={styles["pad-med"]}>
-                        <h2>{make} {model}</h2>
-                    </header>
+            <header className={styles["pad-med"]}>
+                <h2>{make} {model}</h2>
+            </header>
 
-                    <main className={styles["align-center"]}>
-                        <img className={styles["img-thumb"]} src={image} />
-                    </main>
+            <main className={styles["align-center"]}>
+                <img className={styles["img-thumb"]} src={image} />
+            </main>
 
-                    <footer className={`${styles["align-center"]} ${styles["pad-med"]}`}>
-                        <p>Fuel: <strong>{fuel}</strong></p>
-                        <p>Year: <strong>{year}</strong></p>
-                        <p>Price: <strong>{price}$</strong></p>
-                        <a className={styles["action"]} href="/details/:id">See details</a>
-                    </footer>
-                </li>
+            <footer className={`${styles["align-center"]} ${styles["pad-med"]}`}>
+                <p>Fuel: <strong>{fuel}</strong></p>
+                <p>Year: <strong>{year}</strong></p>
+                <p>Price: <strong>{price}$</strong></p>
+                <a className={styles["action"]} href="/details/:id">See details</a>
+            </footer>
+        </li>
     );
 };
 
--- client/src/components/CatalogPage.jsx
+++ client/src/components/CatalogPage.jsx
@@ -1,61 +1,29 @@
 import styles from "./CatalogPage.module.css";
+import { useEffect, useState } from "react";
+import { getAll } from "../services/carServices";
+import CatalogItem from "./CatalogItem";
 
 const CatalogPage = () => {
+    const [cars, setCars] = useState([]);
+
+    useEffect(() => {
+        getAll()
+            .then(result => {
+                const carsEntries = Object.entries(Object.values(result)[0]);
+                // console.log(carsEntries);
+                setCars(carsEntries);
+            });
+    }, []);
+    
     return (
         // <--Catalog Page-->
         <section id="catalog-section" className={styles["spaced"]}>
             <h1 className={styles["item"]}>Auto Occasion</h1>
             <ul className={`${styles["catalog"]} ${styles["cards"]}`}>
-                <li className={styles["item"]}>
-                    <header className={styles["pad-med"]}>
-                        <h2>make model</h2>
-                    </header>
-
-                    <main className={styles["align-center"]}>
-                        <img className={styles["img-thumb"]} src="https://drive.google.com/uc?export=view&id=1V6o8GWLwiDs5h6SlsWjqTBLpZv61l-6t" />
-                    </main>
-
-                    <footer className={`${styles["align-center"]} ${styles["pad-med"]}`}>
-                        <p>Fuel: <strong>fuel</strong></p>
-                        <p>Year: <strong>year</strong></p>
-                        <p>Price: <strong>price$</strong></p>
-                        <a className={styles["action"]} href="/details/:id">See details</a>
-                    </footer>
-                </li>
-
-                <li className={styles["item"]}>
-                    <header className={styles["pad-med"]}>
-                        <h2>make model</h2>
-                    </header>
-
-                    <main className={styles["align-center"]}>
-                        <img className={styles["img-thumb"]} src="https://drive.google.com/uc?export=view&id=1TjXDW46Yc5rcDA9st254HsAiXveRATIW" />
-                    </main>
-
-                    <footer className={`${styles["align-center"]} ${styles["pad-med"]}`}>
-                        <p>Fuel: <strong>fuel</strong></p>
-                        <p>Year: <strong>year</strong></p>
-                        <p>Price: <strong>price$</strong></p>
-                        <a className={styles["action"]} href="/details/:id">See details</a>
-                    </footer>
-                </li>
-
-                <li className={styles["item"]}>
-                    <header className={styles["pad-med"]}>
-                        <h2>make model</h2>
-                    </header>
-
-                    <main className={styles["align-center"]}>
-                        <img className={styles["img-thumb"]} src="https://drive.google.com/uc?export=view&id=1Qz4V1qpizN7hmNUx_gxxzZacnbB7gchw" />
-                    </main>
-
-                    <footer className={`${styles["align-center"]} ${styles["pad-med"]}`}>
-                        <p>Fuel: <strong>fuel</strong></p>
-                        <p>Year: <strong>year</strong></p>
-                        <p>Price: <strong>price$</strong></p>
-                        <a className={styles["action"]} href="/details/:id">See details</a>
-                    </footer>
-                </li>
+                {/* {console.log(cars)} */}
+                {cars.map((car) => 
+                    <CatalogItem key={car[1]['_id']}{...car[1]}/>
+                )}                
             </ul>
             <main className={`${styles["item"]} ${styles["pad-large"]} ${styles["align-center"]}`}>
                 <p>Nothing has been published yet. Be the first!</p>
