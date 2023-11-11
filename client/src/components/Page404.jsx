65057f65833687941d7b2b58a77c26ac4f66302e
JivkoKarakashev <86545491+JivkoKarakashev@users.noreply.github.com>
2023-11-11 12:25:04 +0200

    update 404 Not Found Page

--- client/src/components/Page404.jsx
+++ client/src/components/Page404.jsx
@@ -1,8 +1,11 @@
+import styles from "./Page404.module.css";
+
 const Page404 = () => {
     return (
         <>
             <h1>404 Not Found</h1>
-            <p>The page you&apos;re looking for does not exist.</p>
+            <h2 className={styles["text-center"]}>The page you&apos;re looking for does not exist.</h2>
+            <img className={styles["img-center"]} src="/static/images/404.jpg" />
         </>
     );
 };
